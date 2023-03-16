c626923f26c521bd5abd2a1cf34f43e733050bc6
+ <stingeyandrewstown@live.com>
2023-03-15 19:32:43 -0500

    yes

--- server/app.py
+++ server/app.py
@@ -17,11 +17,28 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        return make_response([p.to_dict() for p in Plant.query.all()], 200)
+
+    def post(self):
+        new_plant = Plant(
+            name=request.get_json()['name'],
+            image=request.get_json()['image'],
+            price=request.get_json()['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        return make_response(Plant.query.filter_by(id=id).first().to_dict(), 200)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
